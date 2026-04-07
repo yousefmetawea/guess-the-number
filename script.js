415b7b0bd8fab10bbe2cad3393c1a9dad8b8e257
yousef metawea <metaweayousef@gmail.com>
2026-04-07 23:06:36 +0200

    refactor code

--- script.js
+++ script.js
@@ -30,10 +30,9 @@ document.querySelector(".number").textContent = secretNumber  ;
     }
 
 
-  }
-  else if(guess >secretNumber ){
+  }else if(guess !== secretNumber){
     if(score >1 ){
-    document.querySelector(".message").textContent = 'too high  ' ; 
+    document.querySelector(".message").textContent =guess>secretNumber ? 'too high  ' : 'too low  ' ; 
     score-- ; 
     document.querySelector(".score").textContent = score ; 
     }else {
@@ -41,16 +40,6 @@ document.querySelector(".number").textContent = secretNumber  ;
     document.querySelector(".score").textContent = 0 ; 
     }
   }
-  else if(guess <secretNumber ){
-    if(score >1 ){
-    document.querySelector(".message").textContent = 'too low  ' ; 
-    score-- ; 
-    document.querySelector(".score").textContent = score ; 
-  }else {
-      document.querySelector(".message").textContent = 'you lost the GAme  ' ; 
-    document.querySelector(".score").textContent = 0 ; 
-    }
-}
 
 })
 
